f04885a8a72302fe650caa0b39e8856d9d9b5b73
saisatish24 <satish2411sai@gmail.com>
2026-06-03 07:51:37 +0530

    Add: n-th-tribonacci-number - submission-3

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int solve(int n ,vector<int> & dp){
+          if ( n == 0){
+            return 0;
+        }
+        if (  n  == 1 || n == 2){
+            return 1;
+        }
+
+        if ( dp[n] != -1){
+            return dp[n];
+        }
+        int ans = 0;
+        dp[n] =   solve(n - 3,dp) + solve(n - 2,dp) + solve( n - 1,dp);
+        return dp[n];
+        
+    }
+
+    int tribonacci(int n) {
+    vector<int> dp ( n + 1 , -1);
+    return solve(n,dp); 
+
+      
+    }
+};
