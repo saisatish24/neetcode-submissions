97ab6cc3d99561be2bca48470411b91ab577821a
saisatish24 <satish2411sai@gmail.com>
2026-08-27 16:39:07 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int solve(vector<vector<int>>& dp, int i, int j, int m, int n) {
+        if (i == (m - 1) && j == (n - 1)) {
+            return 1;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = 0;
+        if (j + 1 < n) ans += solve(dp, i, j + 1, m, n);
+        // (ans += ) because paths from different choices are added together, not replaced.
+
+        if (i + 1 < m) ans += solve(dp, i + 1, j, m, n);
+        return dp[i][j] = ans;
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(dp, 0, 0, m, n);
+    }
+};
